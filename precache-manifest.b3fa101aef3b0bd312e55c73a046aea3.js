e83077923247d521131880f3a84013021b4dbf2b
grzelski <grzelaa@o2.pl>
2021-07-20 18:04:50 +0200

    Updates

---
+++ precache-manifest.b3fa101aef3b0bd312e55c73a046aea3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5270fea3ce900c8bd40b2fd00916ba3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7656c329674a50b6f26",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7192b476479ebc85e903",
+    "url": "/static/css/main.33923105.chunk.css"
+  },
+  {
+    "revision": "e7656c329674a50b6f26",
+    "url": "/static/js/2.3a6e64c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3a6e64c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7192b476479ebc85e903",
+    "url": "/static/js/main.c05702d6.chunk.js"
+  },
+  {
+    "revision": "d55eee123228bcf1f54e",
+    "url": "/static/js/runtime-main.003b1470.js"
+  },
+  {
+    "revision": "e317fdf47dd3b689ef1d183ecf9d7e80",
+    "url": "/static/media/jablka.e317fdf4.jpg"
+  },
+  {
+    "revision": "340e2dd50cd19dd1053d2566c9e1d908",
+    "url": "/static/media/maliny.340e2dd5.jpg"
+  },
+  {
+    "revision": "a5d45a8336874c965477898508d4a669",
+    "url": "/static/media/truskawki.a5d45a83.jpg"
+  }
+]);
