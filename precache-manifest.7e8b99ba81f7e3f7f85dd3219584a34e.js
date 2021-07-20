e2410a34c1baa077fe40c2d7a946419f32e872eb
grzelski <grzelaa@o2.pl>
2021-07-20 18:08:58 +0200

    Updates

---
+++ precache-manifest.7e8b99ba81f7e3f7f85dd3219584a34e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3b48e96cd0f1274de4f7ae34c984f8",
+    "url": "/greengrocer/index.html"
+  },
+  {
+    "revision": "256a43145edf223a4736",
+    "url": "/greengrocer/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "70171da52adddd1238ec",
+    "url": "/greengrocer/static/css/main.33923105.chunk.css"
+  },
+  {
+    "revision": "256a43145edf223a4736",
+    "url": "/greengrocer/static/js/2.1c286fb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/greengrocer/static/js/2.1c286fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70171da52adddd1238ec",
+    "url": "/greengrocer/static/js/main.42055398.chunk.js"
+  },
+  {
+    "revision": "6099782cc9f0c5201ad5",
+    "url": "/greengrocer/static/js/runtime-main.23ec0652.js"
+  },
+  {
+    "revision": "e317fdf47dd3b689ef1d183ecf9d7e80",
+    "url": "/greengrocer/static/media/jablka.e317fdf4.jpg"
+  },
+  {
+    "revision": "340e2dd50cd19dd1053d2566c9e1d908",
+    "url": "/greengrocer/static/media/maliny.340e2dd5.jpg"
+  },
+  {
+    "revision": "a5d45a8336874c965477898508d4a669",
+    "url": "/greengrocer/static/media/truskawki.a5d45a83.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3fa101aef3b0bd312e55c73a046aea3.js"
+  "/greengrocer/precache-manifest.7e8b99ba81f7e3f7f85dd3219584a34e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/greengrocer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
